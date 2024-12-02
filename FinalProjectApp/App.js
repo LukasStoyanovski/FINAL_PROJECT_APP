6f7b32a5e18cb5ec05f3717c9a3fb16114575092
Lukas Stojanovski <lstoyanovski@york.citycollege.eu>
2024-12-03 01:39:18 +0200

    Added Login and Sign up

---
+++ FinalProjectApp/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
---
+++ FinalProjectApp/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the modular Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcYYecmjNVsSWtue5b_9q5NKPIKklnPHo",
+  authDomain: "final-project-app-4081c.firebaseapp.com",
+  projectId: "final-project-app-4081c",
+  storageBucket: "final-project-app-4081c.appspot.com",
+  messagingSenderId: "159231875",
+  appId: "1:159231875:web:d53454780369f54b7750b6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
